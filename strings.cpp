0992dcb8a1762e193304f4c76f2ca5b2ee75ec9e
Adam Manning <adammanning94@gmail.com>
2017-10-31 00:22:10 -0600

    Init

---
+++ strings.cpp
@@ -0,0 +1,27 @@
+#include "strings.hpp"
+
+std::string stdString(System::String^ sysS)
+{
+	std::string stdS = "";
+
+	int i;
+	for (i = 0; i < sysS->Length; i++)
+	{
+		stdS += (char)(sysS->ToCharArray()[i]);
+	}
+
+	return stdS;
+}
+
+System::String^ sysString(std::string& stdS)
+{
+	System::String^ sysS = "";
+
+	int i;
+	for (i = 0; i < stdS.length(); i++)
+	{
+		sysS = sysS->Concat(stdS[i]);
+	}
+
+	return sysS;
+}
---
+++ strings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+std::string stdString(System::String^ sysString);
+System::String^ sysString(std::string& stdS);
